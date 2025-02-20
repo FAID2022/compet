f8481e6816717fcda1fdb782429ec1415368dd45
FAID <faid.yassine02@gmail.com>
2025-02-20 22:19:13 +0100

    first

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+  backendData: string = '';
+  private backendUrl = 'http://localhost:3300';  // Change this based on your Docker setup
+
+  constructor(private http: HttpClient) {}
+
+  getBackendData(): Observable<any> {
+    return this.http.get<any>('http://localhost:3400');
+  }
+
+  ngOnInit(): void {
+    this.getBackendData().subscribe(
+      data => {
+        this.backendData = data.name;  // Assuming the backend returns { name: "messi10" }
+      },
+      error => {
+        console.error('Error fetching data:', error);
+        this.backendData = 'Failed to load data';
+      }
+    );
+  }
+}
